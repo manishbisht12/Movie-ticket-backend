58d7a2f7d327888a17dd33197390bc8713902328
manishbisht12 <154014370+manishbisht12@users.noreply.github.com>
2026-02-03 10:37:40 +0530

    rejectUnauthorized

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -31,6 +31,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    tls: {
+        rejectUnauthorized: false // Handle self-signed certs depending on env
+    }
 });
 
 // Verify connection configuration
